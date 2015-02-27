3df650df3451abaa8dd349afbf3a83f788bce5a4
Alireza Mehrtash <mehrtash@bwh.harvard.edu>
2015-02-27 16:21:21 -0500

    first commit

---
+++ PKModelling/batCalculator.py
@@ -0,0 +1,461 @@
+﻿from __future__ import division
+import SimpleITK as sitk
+from pylab import *
+import numpy as np
+from numpy.linalg import inv
+import scipy.optimize as optimization
+
+class BATCalculator(object):
+    ''' 
+    This is a class for calculating the bolus arrival time
+    with different proposed mehtods in the literature, including
+    L-L model(Linear-Linear model [1]), L-Q (Linear-Quadratic model [1])
+    P-L ( Piecewise-Linear model [2]) and our propsed mehtod [3].
+    ----------------------------------------------------------------
+    Usage:
+    
+    ----------------------------------------------------------------
+    References
+    [1] Cheong, L. H., Koh, T. S., & Hou, Z. (2003). An automatic 
+    approach for estimating bolus arrival time in dynamic contrast 
+    MRI using piecewise continuous regression models. 
+    Physics in Medicine and Biology, 48(5), N83–8.
+    [2] Singh, A., Rathore, R. K. S., Haris, M., Verma, S. K., Husain
+    , N., & Gupta, R. K. (2009). Improved bolus arrival time and arterial
+    input function estimation for tracer kinetic analysis in DCE-MRI. 
+    Journal of Magnetic Resonance Imaging : JMRI, 29(1), 166–76. 
+    doi:10.1002/jmri.21624
+    '''     
+    def __init__ (self, method = 'L-L'):
+        self.method = method
+        self.c = []
+        self.BSIterationNumber = 10
+    def calculate(self):
+        output = -1, []
+        if len(self.c) < 2:
+            print "Error: Assign a concentration curve!"
+        else:
+            if self.method == 'LL':
+                output  = self.__calculateBATLLMethod(self.c)
+            elif self.method == 'LQ':
+                output  = self.__calculateBATLQMethod(self.c)
+            elif self.method == 'PL':
+                output  = self.__calculateBATPLMethod(self.c)
+            elif self.method == 'PLBS':
+                output  = self.__calculateBATPLBSMethod(self.c)
+            elif self.method == 'XY': #TODO: Assign an appropriate name
+                output  = self.__calculateBATXYMethod(self.c)
+            elif self.method == 'XYBS': #TODO: Assign an appropriate name
+                output  = self.__calculateBATXYBSMethod(self.c)
+            elif self.method == 'PG': #TODO: Assign an appropriate name
+                output  = self.__calculateBATPGMethod(self.c)
+            else:
+                print 'Error: Methods are LL, LQ, PL, XY and XYBS'
+            return output
+
+    def __calculateBATPGMethod (self, c):
+      i = -1
+      bat = -1
+      maxIndex = -1
+      c = c.astype('float')
+      cprime = np.diff(c)
+      # finding the peak of gradient
+      maxValue = np.amax(cprime)
+      maxIndex = np.where(cprime == maxValue)[0][0]
+      skip1 = 0
+      thresh = maxValue / 10.0
+      for i in xrange(maxIndex, skip1,-1):
+          if(cprime[i] < thresh):
+              break
+      if i != -1:
+        bat = i+1;
+      return bat, maxIndex
+
+    def __calculateBATLLMethod(self,c):
+        bat, beta, p = self.calculateBATLLModel(c)
+        cExpected = self.LLFunction(bat, beta, p)
+        return bat, cExpected
+
+    def __calculateBATLQMethod(self,c):
+        bat, beta, p = self.calculateBATLQModel(c)
+        cExpected = self.LQFunction(bat, beta, p)
+        return bat, cExpected
+    
+    def __calculateBATPLMethod(self,c):
+        n = len(c)
+        alpha, beta, h = self.calculateBATPLModel(c)
+        cExpected = self.PLFunction(alpha, beta, h,n) 
+        return alpha, cExpected
+     
+    def __calculateBATPLBSMethod(self,c):
+        n = len(c)
+        alpha, beta, h = self.calculateBATPLModel(c)
+        alpha, beta = self.midPointIterationsPL(c,alpha,beta,self.BSIterationNumber)
+        cExpected = self.PLFunction(alpha, beta, h,n) 
+        return alpha, cExpected
+    
+    def __calculateBATXYMethod(self,c):
+        bat, beta, a,b = self.calculateBATXYModel(c)
+        cExpected = self.LLFunction(bat, beta, b)
+        return bat, cExpected
+
+    def __calculateBATXYBSMethod(self,c):
+        bat, beta, a,b = self.calculateBATXYModel(c)
+        bat = self.midPointIterationsXY(c[a:b],bat-a,self.BSIterationNumber)+a
+        cExpected = self.LLFunction(bat, beta, b)
+        return bat, cExpected
+
+    def calculateBATLLModel (self,c):
+        q = np.where(c == max(c))
+        p = q[0][0]
+        beta = [-1,-1]
+        if p > 1 :
+            c = c[0:p]
+            c.reshape(p,1)        
+            # find the index of maximum element in array
+            sse = []
+            betaValues = []
+            #Step 0: Let p be the index to the maximum element in array C.
+            #Step 1: For k = 1to p − 1, do steps 1.1 to 1.3.
+            for k in xrange(0,p-1):
+                sse_k , beta = self.calculateSSEkLLModel(c,k,p) 
+                betaValues.append(beta)
+                sse.append(sse_k)
+            #Step 2: Let kmin be the index to the minimum element in array SSE, then the estimated BAT is t[kmin].2.2.
+            #print sse
+            #print "Bolus arrival time is:", 
+            sse_min = np.amin(sse)
+            #print sse_min
+            bat = np.where(sse == sse_min)[0][0]
+            beta = betaValues[bat]
+            return bat, beta, p
+        else:
+            #print 'Calculating LL max index is less than 1'
+            bat = -1
+            return bat,beta,p
+
+    def calculateSSEkLLModel( self, c, k, p):
+        #1.1. Construct X.
+        x = np.zeros((p,2))
+        for i in xrange(0,p):
+            x[i,0] = 1
+            if i<= k:
+                x[i,1] = 0
+            else:
+                x[i,1] = i-k
+        #1.2. Solve equation (4)for βlsq (using eithermatrix inversion or constrained least- squares methods).
+        #print x
+        #print c
+        beta = np.dot(inv(np.dot(np.transpose(x),x)),np.dot(np.transpose(x),c))
+        # print "beta is:", beta
+        #1.3. Compute SSE [k].
+        sse_k = np.dot(np.transpose((c-np.dot(x,beta))),(c-np.dot(x,beta)))
+        return sse_k, beta      
+
+    def LLFunction (self, bat, beta, p):
+        x = np.zeros((p,2))
+        for i in xrange(0,p):
+            x[i,0] = 1
+            if i <= bat:
+                x[i,1] = 0
+            else:
+                x[i,1] = i-bat
+        return np.dot(x,beta)
+
+    def LLCostFunction (self, beta, x, c):
+        return c-np.dot(x,beta)
+  
+    def calculateBATLLModel_LSQ (self, c):
+        p = np.where(c == max(c))[0][0]
+        if p > 2:
+            c = c[0:p]
+            c.reshape(p,1)
+            # find the index of maximum element in array
+            sse = []
+            #Step 0: Let p be the index to the maximum element in array C.
+            #Step 1: For k = 1to p − 1, do steps 1.1 to 1.3.
+            for k in xrange(0,p-1):
+                #1.1. Construct X.
+                x = np.zeros((p,2))
+                for i in xrange(0,p):
+                    x[i,0] = 1
+                    if i<= k:
+                        x[i,1] = 0
+                    else:
+                        x[i,1] = i-k
+                #1.2. Solve equation (4)for βlsq (using eithermatrix inversion or constrained least- squares methods).
+                beta0 = np.ones(2)
+                beta0.reshape(2,1)
+                beta = optimization.leastsq(self.LLCostFunction, beta0, args=(x, c))[0]
+                beta.reshape(2,1)
+                #print beta
+                #print "beta is:", beta
+                #1.3. Compute SSE [k].
+                sse_k = np.dot(np.transpose((c-np.dot(x,beta))),(c-np.dot(x,beta)))
+                #print sse_k
+                #print "SSE is:", sse_k[0,0]
+                sse.append(sse_k)
+            #Step 2: Let kmin be the index to the minimum element in array SSE, then the estimated BAT is t[kmin].2.2.
+            #print sse
+            #print "Bolus arrival time is:", 
+            sse_min = np.amin(sse)
+            #print sse_min
+            bat = np.where(sse == sse_min)[0][0]
+            return bat, beta, p
+        else:
+            #print 'concentration curve is not appropriate'
+            bat = -1     
+            return bat
+
+    def LQFunction (self, bat, beta, p):
+        x = np.zeros((p,3))
+        for i in xrange(0,p):
+            x[i,0] = 1
+            if i <= bat:
+                x[i,1] = 0
+                x[i,2] = 0
+            else:
+                x[i,1] = i-bat
+                x[i,2] = square(i-bat)
+        return np.dot(x,beta)
+
+    def LQCostFunction (self, beta, x, c):
+        return c-np.dot(x,beta)
+
+    def calculateBATLQModel (self, c):
+        p = np.where(c == max(c))[0][0]
+        beta = [-1,-1,-1]
+        if p >2 :
+            c = c[0:p]
+            c.reshape(p,1)
+            betaValues = []
+            sse = []
+            for k in xrange(0,p-1):
+                x = np.zeros((p,3))
+                for i in xrange(0,p):
+                    x[i,0] = 1
+                    if i<= k:
+                        x[i,1] = 0
+                        x[i,2] = 0
+                    else:
+                        x[i,1] = i-k
+                        x[i,2] = square(i-k)
+                beta0 = np.ones(3)
+                beta0.reshape(3,1)
+                # Without constraints
+                #'''
+                beta = optimization.leastsq(self.LQCostFunction, beta0, args=(x, c))[0]
+                #print beta
+
+                '''
+                if beta[1]< 0: 
+                    print "negative beta1"
+                if  beta[2]<0:
+                    #print "negative beta2"
+                    
+                '''
+                '''
+                bounds = np.array([[-inf,inf], [0, inf], [0,inf]])
+                #print bounds
+                #With constraints
+                beta =leastsq_bounds( LQFunction, beta0, bounds, 10, args=(x, c))[0]
+                #print beta
+                '''
+                beta.reshape(3,1)
+                sse_k = np.dot(np.transpose((c-np.dot(x,beta))),(c-np.dot(x,beta)))
+                #print sse_k
+                #print "SSE is:", sse_k[0,0]
+                sse.append(sse_k)
+                betaValues.append(beta)
+            #Step 2: Let kmin be the index to the minimum element in array SSE, then the estimated BAT is t[kmin].2.2.
+            #print sse
+            #print "Bolus arrival time is:", 
+            sse_min = np.amin(sse)
+            #print sse_min
+            minIndex = np.where(sse == sse_min)[0]
+            if minIndex:
+              bat = minIndex[0]
+            else:
+              bat = -1
+            beta = betaValues[bat]
+            return bat, beta, p
+        else:
+            print 'concentration curve is not appropriate'
+            bat = -1
+            return bat,beta,p
+
+    def PLFunction (self, alpha, beta , h, n):
+        x = np.zeros((n,3))
+        for i in xrange(0,n):
+            x[i,0] = 1
+            if i <= alpha:
+                x[i,1] = 0
+                x[i,2] = 0
+            elif i > alpha and i <= beta:
+                x[i,1] = i-alpha
+                x[i,2] = 0
+            elif i> beta:
+                x[i,1] = beta-alpha
+                x[i,2] = i - beta
+        return np.dot(x,h)
+
+    def PLCostFunction (self, h, x, c):
+        return c-np.dot(x,h)
+
+    def calculateBATPLModel (self, c):
+        n = len(c)
+        if n >2 :
+            c.reshape(n,1)
+            params = []
+            sse = []
+            for alpha in xrange(0,n-1):
+                for beta in xrange(alpha,n):
+                    sse_k,h = self.calculateSSEkPLModel(c, alpha, beta,n)
+                    sse.append(sse_k)
+                    params.append([alpha, beta, h])
+            #Step 2: Let kmin be the index to the minimum element in array SSE, then the estimated BAT is t[kmin].2.2.
+            #print sse
+            #print "Bolus arrival time is:", 
+            sse_min = np.amin(sse)
+            #print sse_min
+            sse_minIndex = np.where(sse == sse_min)[0][0]
+            return params[sse_minIndex]
+        else:
+            print 'concentration curve is not appropriate'
+            bat = -1 
+            return bat
+      
+    def calculateSSEkPLModel(self, c, alpha, beta,n):
+        x = np.zeros((n,3))
+        for i in xrange(0,n):
+            x[i,0] = 1
+            if i <= alpha:
+                x[i,1] = 0
+                x[i,2] = 0
+            elif i > alpha and i <= beta:
+                x[i,1] = i-alpha
+                x[i,2] = 0
+            elif i> beta:
+                x[i,1] = beta-alpha
+                x[i,2] = i - beta
+
+        h0 = np.ones(3)
+        h0.reshape(3,1)
+        h = optimization.leastsq(self.PLCostFunction, h0 , args=(x, c))[0]
+        '''
+        #print beta
+        if h[1]< 0 or h[2]<0:
+            print "negative beta1 or beta2"
+        bounds = np.array([[-inf,inf], [0, inf], [0,inf]])
+        #print bounds
+        #With constraints
+        beta =leastsq_bounds( LQFunction, beta0, bounds, 10, args=(x, c))[0]
+        #print beta
+        '''
+        h.reshape(3,1)
+        sse_k = np.dot(np.transpose((c-np.dot(x,h))),(c-np.dot(x,h)))
+        return sse_k, h
+ 
+    def midPointIterationsPL(self, c, alpha, beta, iterationNumber):
+        #n is the number of iterations
+        n = len(c)
+        currentAlpha = float(alpha)
+        currentBeta = beta
+        currentStepSize = 1
+        for i in xrange(0,iterationNumber):
+            # for alpha
+            rightShift = float(currentAlpha + currentStepSize/2)
+            leftShift = float(currentAlpha - currentStepSize/2)
+            rightError = self.calculateSSEkPLModel(c, rightShift,currentBeta,n)[0]
+            leftError = self.calculateSSEkPLModel(c, leftShift, currentBeta,n)[0]
+            if rightError < leftError:
+                currentAlpha = rightShift
+            else:
+                currentAlpha = leftShift
+            # for beta
+            rightShift = float(currentBeta) + currentStepSize/2
+            leftShift = float(currentBeta) - currentStepSize/2
+            rightError = self.calculateSSEkPLModel(c, currentAlpha, rightShift,n)[0]
+            leftError = self.calculateSSEkPLModel(c, currentAlpha, leftShift, n)[0]
+            if rightError < leftError:
+                currentBeta = rightShift
+            else:
+                currentBeta = leftShift
+            currentStepSize = currentStepSize/2
+        return currentAlpha, currentBeta
+ 
+    def calculateBATXYModel(self,c):
+        c = c.astype('float')
+        p = len(c)
+        x = linspace(0,p-1,p)
+        normalizedC = (c- min(c))/(max(c)-min(c))
+        normalizedCMean = np.mean(normalizedC)
+        # 
+        # Calculate Derrivative of c and normalization
+        #
+        cprime = np.diff(c)
+        # finding the peak of gradient
+        peakGradientIndex = np.where(cprime == max(cprime))[0]
+        '''
+        normalizedCPrime = (cprime- min(cprime))/(max(cprime)-min(cprime))
+        normalizedCPrimeMean = np.mean(normalizedCPrime)
+        vicinity_index = -1
+        movingAvC = c[0]
+        maximumC = np.max(c)
+        minimumC = np.min(c)
+        #
+        # Calculating vicinity_index (an estimation of BAT) using peak gradient and jump from moving average
+        #
+        for index,element in enumerate(normalizedCPrime):
+            movingAvC = (movingAvC + c[index])/2
+            if ((c[index] - movingAvC) > 0.05*(maximumC-minimumC) and  element > normalizedCPrimeMean *1.2): 
+                vicinity_index = index        
+                break
+        # Making the vicinity window
+        '''
+        windowSize =  int(len(c)/5)
+        #print 'window size is: ', windowSize
+        vicinity_index = peakGradientIndex
+        #print 'vicinity index is: ', vicinity_index
+        if (vicinity_index - windowSize)<0:
+            a = 0
+        else:
+            a = vicinity_index - windowSize
+        b = int(vicinity_index + windowSize/2)
+        #print b
+        #
+        # Create the new signal in the vicinity of BAT (cBATVicinity)
+        #
+        cBATVicinity = c[a:b]
+        maxCBATVic = max(cBATVicinity)
+        # remove declining values
+        maxIndex = np.where(cBATVicinity == maxCBATVic)[0][0]
+        b = a + maxIndex + 1
+        cBATVicinity = c[a:b]
+        #print cBATVicinity
+        if len(cBATVicinity) > 5:
+            # Using the Linear-Linear model to calculate the BAT which minimizes the SSE
+            [calcualtedBAT, beta, p]  = self.calculateBATLLModel(cBATVicinity)
+        else:
+            print "cBATVicinity has less than 5 samples"
+            [calcualtedBAT, beta, p]  = [0,0,0]
+        bat = a + calcualtedBAT
+        return bat,beta, a, b
+
+    def midPointIterationsXY(self, c, bat, iterationNumber):
+        #n is the number of iterations
+        n = len(c)
+        currentBAT = bat
+        currentStepSize = 1
+        for i in xrange(0,iterationNumber):
+            rightShift = float(currentBAT + currentStepSize/2)
+            leftShift = float(currentBAT - currentStepSize/2)
+            rightError = self.calculateSSEkLLModel(c, rightShift,n)[0]
+            leftError = self.calculateSSEkLLModel(c, leftShift, n)[0]
+
+            if rightError < leftError:
+                currentBAT = rightShift
+            else:
+                currentBAT = leftShift
+            currentStepSize = currentStepSize/2
+        return currentBAT
---
+++ PKModelling/pkModelling.py
@@ -0,0 +1,267 @@
+from __future__ import division
+from pylab import *
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import SimpleITK as sitk
+from scipy.optimize import curve_fit
+from scipy import stats
+
+# Signal to concentration converter
+class SignalToConcentrationConverter:
+    '''
+    This is a class to convert the MR signal to bolus concentration curve.
+    Usage
+    
+    References
+    [1]
+    '''
+    def __init__ (self):
+        self.signal = []
+        self.TR = None # Repetition Time
+        self.FA = None # Flip Angle
+        self.T10 = None # T1 before injection of CA
+        self.Relaxivity = None
+        self.BAT = None
+
+    def convert(self):
+        c = []
+        if len(self.signal) > 0:
+            # TODO: For real signals replace with averaging
+            s0 = self.signal[0]
+            s0 = mean(self.signal[:self.BAT])
+            if s0 < 1 :
+                s0 = 1
+                print 's0 is <1, s0 was assigned as 1'
+                #print self.signal
+
+            for s in self.signal:
+                exp_TR_BloodT1 = exp(-self.TR/self.T10)
+                alpha = self.FA * pi/180
+                cos_alpha = cos(alpha)
+                constB = (1- exp_TR_BloodT1)/(1-cos_alpha*exp_TR_BloodT1)
+                constA = s/s0
+                value = (1 - constA*constB)/(1- constA*constB*cos_alpha)
+                if value < 0:
+                    concentration = 0
+                else:
+                    log_value = log(value)
+                    ROft = (-1/self.TR)*log_value
+                    Cb = (ROft  - (1/self.T10))/ self.Relaxivity
+                    if (Cb < 0):
+                        Cb = 0
+                    concentration = Cb
+                c.append(concentration)
+        else:
+            print 'you must provide an appropriate signal'
+        return np.array(c)
+
+class PKSolver(object):
+    '''
+    This is a class to solve the PK Modelling problem with Tofts model.
+    Usage
+    
+    References
+    [1]
+    '''
+    samplingRate = None
+    hematocrit= None
+    rSquarred = 0
+
+    def __init__(self):
+        self.__AIF = []
+        self.__BATAIF = 0
+        self.__BATTRF = 0
+        self.__TRF = []
+        self.__AIFShifted = []
+        self.__TRFShifted = []
+        self.__ktrans = -1
+        self.__ve = -1
+
+    def getAIF(self):
+        return self.__AIF
+
+    def setAIF(self, val):
+        self.__AIF = val
+        self.__setBAT()
+
+    AIF = property(getAIF, setAIF)
+
+    def getTRF(self):
+        return self.__TRF
+
+    def setTRF(self, val):
+        self.__TRF = val 
+        self.__setBAT()
+
+    TRF = property(getTRF, setTRF)
+
+    def getBATAIF(self):
+        return self.__BATAIF
+
+    def setBATAIF(self, val):
+        self.__BATAIF = val
+        self.__setBAT()
+
+    BATAIF = property(getBATAIF, setBATAIF)
+
+    def getBATTRF(self):
+        return self.__BATTRF
+    
+    def setBATTRF(self, val):
+        self.__BATTRF = val
+        self.__setBAT()
+
+    BATTRF = property(getBATTRF, setBATTRF)
+
+    def func(self,t,Ktrans,ve):
+        result = None
+        if self.mode == 'shifted':
+            p = len(self.__AIFShifted)
+            t = linspace(0, self.samplingRate*(p-1),p)/60
+            delta= t[1] - t[0]
+            result = (1/(1-self.hematocrit))*Ktrans*(
+                    convolve(self.__AIFShifted,exp(-Ktrans/ve*t))*delta)[0:len(t)]
+        else:
+            p = len(self.__AIF)
+            t = linspace(0, self.samplingRate*(p-1),p)/60
+            delta= t[1] - t[0]
+            result = (1/(1-self.hematocrit))*Ktrans*(
+                    convolve(self.__AIF,exp(-Ktrans/ve*t))*delta)[0:len(t)]   
+        return result
+
+    def solve(self):
+        p = len(self.__AIFShifted)
+        t = linspace(0, self.samplingRate*(p-1),p)
+        #t = self.__t[:]/60  # ktrans is in min-1
+        self.mode = 'shifted'
+        try:
+            popt, pcov= curve_fit(self.func, t, self.__TRFShifted,np.array([0.1,1]))
+            if popt[1] < 0 or popt[1]>10:
+              popt, pcov= curve_fit(self.func, t, self.__TRFShifted,np.array([0.1,0.1]))
+            if popt[1] < 0 or popt[1]>10:
+              popt, pcov= curve_fit(self.func, t, self.__TRFShifted,np.array([0.1,0.5]))
+            if popt[1] < 0 or popt[1]>10:
+              popt, pcov= curve_fit(self.func, t, self.__TRFShifted,np.array([0.1,0.9]))
+            if popt[1] < 0 or popt[1]>10:
+              popt, pcov= curve_fit(self.func, t, self.__TRFShifted,np.array([1,0.8]))
+            if popt[1] < 0 or popt[1]>10:
+              popt, pcov= curve_fit(self.func, t, self.__TRFShifted,np.array([1,0.2]))
+            if popt[1] < 0 or popt[1]>10:
+              popt, pcov= curve_fit(self.func, t, self.__TRFShifted,np.array([1,0.4]))
+            if popt[1] < 0 or popt[1]>10:
+              popt[1] = -1
+        except Exception, err:
+          print Exception, err
+          #print 'shifted TRF is: ', self.__TRFShifted
+          popt = [-1, -1]
+          pcov = [-1,-1]
+
+        #perr = np.sqrt(np.diag(pcov))
+        perr = [-1, -1]
+        [self.__ktrans,self.__ve] = popt
+        [self.__ktransError,self.__veError] = perr
+        '''
+        if self.__ve < 0:
+          self.__ve = -1
+        if self.__ktrans < 0:
+          self.__ktrans = -1
+        '''
+        if self.__ve == -1 or self.__ktrans == -1:
+          self.__ve = -1
+          self.__ktrans = -1
+        return np.array([self.__ktrans, self.__ve,self.__ktransError,self.__veError])
+
+    def calculateGoodnessOfFit(self):
+        if self.__ktrans != -1 and self.__ve != -1:                    
+            p = len(self.__TRFShifted)
+            t = linspace(0, self.samplingRate*(p-1),p)/60
+            observed = self.__TRFShifted
+            #self.mode = 'original'
+            self.mode = 'shifted'
+            expected = self.func(t,self.__ktrans,self.__ve)
+            self.rSquarred = 0
+            slope, intercept, r_value, p_value, std_err = stats.linregress(observed,expected) 
+            self.rSquarred = r_value**2
+        else:
+            self.rSquarred = -1
+        return self.rSquarred
+
+    def plotResults(self):
+        if self.__ktrans != -1:
+            p = len(self.__TRFShifted)
+            t = linspace(0, self.samplingRate*(p-1),p)/60
+            fig, ax= plt.subplots()
+            fig.set_size_inches(16,4)
+            ax1 = plt.subplot(1,2,1)
+            ax1.grid(True)
+            plot(t, self.__AIFShifted,'b--',label='AIF')
+            plot(t, self.__TRFShifted*3,'r--',label='TRF*3')
+            plt.legend(loc=0,prop={'size':14})
+            ax2 = plt.subplot(1,2,2)
+            ax2.grid(True)
+            p = len(self.__TRFShifted)
+            t = linspace(0, self.samplingRate*(p-1),p)/60
+            plot(t , self.__TRFShifted,'o' ,mfc='w',mec='b',ms=6 ,label='observed')
+            self.mode = 'shifted'
+            fitted = self.func(t , self.__ktrans, self.__ve)
+            plot(t, fitted, 'r--',label='fitted')
+            plt.legend(loc=0,prop={'size':14})
+            rsquared = self.calculateGoodnessOfFit()
+            title('ktrans: ' + str(self.__ktrans) +  
+                  ', ve: ' + str(self.__ve)  + 
+                  ', R2: '+ str(rsquared))
+        else:
+            print 'plot error: you must run solver first'
+
+    def __setBAT(self):
+        # Apply fractional-shift if BAT is non-integer
+        diff = self.__BATTRF - int(self.__BATTRF) 
+        if diff != 0 :
+            TRF = self.__applyFractionalShift(self.__TRF, diff)
+            BATTRF = int(self.__BATTRF)
+        else:
+            TRF = self.__TRF
+            BATTRF = self.__BATTRF
+
+        diff = self.__BATAIF - int(self.__BATAIF) 
+        if diff != 0 :
+            print 'warning: non integer shift for aif'
+            AIF = self.__applyFractionalShift(self.__AIF, diff)
+            BATAIF = int(self.__BATAIF)
+        else:
+            AIF = self.__AIF
+            BATAIF = int(self.__BATAIF)
+
+        # Applying the integer part of the shift
+        if BATTRF > len(self.__TRF) or BATAIF > len(self.__AIF):
+            print 'Error: You must assign smaller BAT or assigng appropriate concentration signals first!'
+            print 'BATTRF is:', BATTRF
+            print 'len TRF is:', len(self.__TRF)
+            print 'BATAIF is:', BATAIF
+            print 'len AIF is:', len(self.__AIF)
+
+        else:
+            if BATTRF > BATAIF:
+                padding = BATTRF - BATAIF
+                self.__TRFShifted = TRF[BATTRF:]
+                self.__AIFShifted = AIF[BATAIF:-padding]
+            elif BATTRF < BATAIF:
+                padding = BATAIF - BATTRF
+                self.__TRFShifted = TRF[BATTRF:-padding]
+                self.__AIFShifted = AIF[BATAIF:]
+            else:
+                self.__TRFShifted = TRF[BATTRF:]
+                self.__AIFShifted = AIF[BATAIF:]
+
+    def __applyFractionalShift(self,c, diff):
+        l = len(c)
+        if l> 0:
+          cDelayed = zeros(l)
+          for i in xrange(0,l-1):
+             cDelayed[i] = (1-diff)*c[i] + diff*c[i+1]
+          # TODO: check if the following line is correct
+          cDelayed[l-1] = c[l-1]
+        else:
+          cDelayed = c
+        return cDelayed
